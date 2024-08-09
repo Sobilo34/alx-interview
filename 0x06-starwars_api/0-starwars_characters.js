cebb3cafaf74f8a7970f02cce297e2d635f49ef1
Sobilo34 <bilalsolih60@gmail.com>
2024-08-09 04:37:24 +0100

    Star wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/**
+ * Starwars API
+ */
+const req = require('request');
+
+const movieId = process.argv[2];
+const movieLink = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+req(movieLink, (err, res, body) => {
+  if (err) console.log(err);
+  const i = 0;
+  const characters = JSON.parse(body).characters;
+  movieChar(characters, i);
+});
+
+const movieChar = function (url, i) {
+  req(url[i], (err, res, body) => {
+    if (err) console.log(err);
+    console.log(JSON.parse(body).name);
+    if (++i < url.length) {
+      movieChar(url, i++);
+    }
+  });
+};
